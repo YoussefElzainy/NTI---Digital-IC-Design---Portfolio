685f6b9745f7720240e42f20a2d5ee1a00ad7d03
youssef elzainy <youssefhoss79@gmail.com>
2026-09-08 23:05:46 +0300

    Add files via upload

--- Week Two/Day 8 Labs/Data Driver/DataDriver.v
+++ Week Two/Day 8 Labs/Data Driver/DataDriver.v
@@ -1,1 +1,11 @@
+module driver #(
+    parameter WIDTH = 8;
+)(
+    input [WIDTH - 1: 0] data_in,
+    input data_en, 
+    output [WIDTH - 1: 0] data_out
+);
 
+    assign data_out = data_en? data_in : {(WIDTH){z}};
+
+endmodule

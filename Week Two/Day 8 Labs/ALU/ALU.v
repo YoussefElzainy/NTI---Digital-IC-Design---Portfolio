68df658336102935505990fd101705162b97e601
youssef elzainy <youssefhoss79@gmail.com>
2026-09-08 22:33:49 +0300

    Add files via upload

--- Week Two/Day 8 Labs/ALU/ALU.v
+++ Week Two/Day 8 Labs/ALU/ALU.v
@@ -1,7 +1,7 @@
 module alu (
     input [7 : 0] in_a, in_b,
     input [2 : 0] opcode, 
-    output reg [WIDTH - 1 : 0] alu_out,
+    output reg [7 : 0] alu_out,
     output reg a_is_zero
 );
     
@@ -13,7 +13,7 @@ module alu (
             3'b010: alu_out = in_a + in_b;
             3'b011: alu_out = in_a & in_b;
             3'b100: alu_out = in_a ^ in_b;
-            3'b101: alu_out =  alu_out = in_b;
+            3'b101: alu_out = in_b;
             3'b110: alu_out = in_a;
             3'b111: alu_out = in_a;
             default: alu_out = 0;
